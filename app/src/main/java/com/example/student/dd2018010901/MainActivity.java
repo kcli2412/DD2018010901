8d297782bca92aaea3c75f8a92612aba04cd7bfc
kcli2412 <kcli2412@gmail.com>
2018-01-09 11:47:16 +0800

    create Menu

--- app/src/main/java/com/example/student/dd2018010901/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018010901/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.student.dd2018010901;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("ADD");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle().equals("ADD"))
+        {
+            Toast.makeText(MainActivity.this, "使用者按下 ADD", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
